67c07a2500b0dbb3ab1cd45d90fe0bab98612b4c
Dominik Bylica <dominikjacekbylica@gmail.com>
2014-11-18 02:12:30 +0100

    Fix accessing devise by json.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include ActionController::ImplicitRender
   include Devise::Controllers::SignInOut
   extend SimpleTokenAuthentication::ActsAsTokenAuthenticationHandler
+
+  respond_to :json
 end
